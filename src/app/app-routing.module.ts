32aa7fc1164eaff9769be534aca6afcc38cd28e9
Lukas <boehm-lukas@outlook.de>
2021-07-15 05:06:55 +0200

    :sparkles:  add basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { DownloadComponent } from "./download/download.component";
+import { UploadComponent } from "./upload/upload.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+	{ path: "upload", component: UploadComponent },
+	{ path: "download/:id", component: DownloadComponent },
+	{ path: "**", redirectTo: "upload" },
+];
 
 @NgModule({
 	imports: [RouterModule.forRoot(routes)],

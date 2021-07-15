07a3c28515e596e441cf90cb7f778459997701b9
Lukas <boehm-lukas@outlook.de>
2021-07-15 05:04:07 +0200

    create some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { UploadComponent } from './upload/upload.component';
+import { DownloadComponent } from './download/download.component';
 
 @NgModule({
-	declarations: [AppComponent],
+	declarations: [AppComponent, UploadComponent, DownloadComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],

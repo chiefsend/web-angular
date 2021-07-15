ca68a9fd3dc1b51e350ae5acb6537b02f0e6823c
Lukas <boehm-lukas@outlook.de>
2021-07-15 05:11:15 +0200

    :sparkles: basic header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { UploadComponent } from './upload/upload.component';
 import { DownloadComponent } from './download/download.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-	declarations: [AppComponent, UploadComponent, DownloadComponent],
+	declarations: [AppComponent, UploadComponent, DownloadComponent, HeaderComponent, FooterComponent],
 	imports: [BrowserModule, AppRoutingModule],
 	providers: [],
 	bootstrap: [AppComponent],

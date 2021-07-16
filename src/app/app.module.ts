57eebb1825dcae0e0413407544927328158b61dd
Lukas <boehm-lukas@outlook.de>
2021-07-16 16:44:24 +0200

    :truck: import material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { HttpClientModule } from "@angular/common/http";
 
+import { MaterialModule } from "./material/material.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { UploadComponent } from "./upload/upload.component";
@@ -9,14 +10,28 @@ import { DownloadComponent } from "./download/download.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { ApiService } from "./api/api.service";
-import { PublicComponent } from './public/public.component';
-import { HomeComponent } from './home/home.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PublicComponent } from "./public/public.component";
+import { HomeComponent } from "./home/home.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
-	declarations: [AppComponent, UploadComponent, DownloadComponent, HeaderComponent, FooterComponent, PublicComponent, HomeComponent],
-	imports: [BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
-	providers: [ApiService],
-	bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    UploadComponent,
+    DownloadComponent,
+    HeaderComponent,
+    FooterComponent,
+    PublicComponent,
+    HomeComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+  ],
+  providers: [ApiService],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}

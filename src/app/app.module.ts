7489a75acd624328971de8065bb5127e795c57b3
Lukas <boehm-lukas@outlook.de>
2021-07-16 08:27:05 +0200

    :fire: autoformat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { UploadComponent } from './upload/upload.component';
-import { DownloadComponent } from './download/download.component';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
+import { UploadComponent } from "./upload/upload.component";
+import { DownloadComponent } from "./download/download.component";
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @NgModule({
 	declarations: [AppComponent, UploadComponent, DownloadComponent, HeaderComponent, FooterComponent],
--- src/app/upload/upload.component.ts
+++ src/app/upload/upload.component.ts
@@ -1,15 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-upload',
-  templateUrl: './upload.component.html',
-  styleUrls: ['./upload.component.css']
+	selector: "app-upload",
+	templateUrl: "./upload.component.html",
+	styleUrls: ["./upload.component.css"],
 })
 export class UploadComponent implements OnInit {
+	constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }

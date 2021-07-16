2e5aa6c4e53dda190a11b8b64e8655917a59c794
Lukas <boehm-lukas@outlook.de>
2021-07-16 08:48:29 +0200

    :construction: basic API service

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,8 +1,45 @@
+import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { environment } from "src/environments/environment";
+import { Attachment, Share } from "./models";
 
 @Injectable({
 	providedIn: "root",
 })
 export class ApiService {
-	constructor() {}
+	apiUrl: string = environment.baseURL;
+
+	constructor(private http: HttpClient) {}
+
+	getAllShares(): Observable<Share[]> {
+		return this.http.get<Share[]>(`${this.apiUrl}/shares`);
+	}
+
+	createShare(share: Share): Observable<Share> {
+		return this.http.post<Share>(`${this.apiUrl}/shares`, share);
+	}
+
+	getShare(id: string): Observable<Share> {
+		return this.http.get<Share>(`${this.apiUrl}/share/${id}`);
+	}
+
+	closeShare(id: string): Observable<Share> {
+		return this.http.post<Share>(`${this.apiUrl}/share/${id}`, null);
+	}
+
+	zipShare(id: string): Observable<any> {
+		// FIXME return type
+		return this.http.get(`${this.apiUrl}/share/${id}/zip`);
+	}
+
+	uploadAttachment(id: string, file: any): Observable<Attachment> {
+		// FIXME type of param file
+		return this.http.post<Attachment>(`${this.apiUrl}/share/${id}/attachments`, file);
+	}
+
+	downloadAttachment(id: string, att: string): Observable<any> {
+		// FIXME return type
+		return this.http.get(`${this.apiUrl}/share/${id}/attachment${att}`);
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -7,11 +8,12 @@ import { UploadComponent } from "./upload/upload.component";
 import { DownloadComponent } from "./download/download.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
+import { ApiService } from "./api/api.service";
 
 @NgModule({
 	declarations: [AppComponent, UploadComponent, DownloadComponent, HeaderComponent, FooterComponent],
-	imports: [BrowserModule, AppRoutingModule],
-	providers: [],
+	imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+	providers: [ApiService],
 	bootstrap: [AppComponent],
 })
 export class AppModule {}

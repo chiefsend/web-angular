f1ce0c5e884c80bbfdb6bc543468745b5d7408f2
Lukas <boehm-lukas@outlook.de>
2021-07-16 16:24:56 +0200

    :sparkles: :construction: add new components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { DownloadComponent } from "./download/download.component";
+import { HomeComponent } from "./home/home.component";
+import { PublicComponent } from "./public/public.component";
 import { UploadComponent } from "./upload/upload.component";
 
 const routes: Routes = [
 	{ path: "upload", component: UploadComponent },
 	{ path: "download/:id", component: DownloadComponent },
-	{ path: "**", redirectTo: "upload" },
+	{ path: "public", component: PublicComponent },
+	{ path: "**", component: HomeComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,11 @@ import { DownloadComponent } from "./download/download.component";
 import { HeaderComponent } from "./header/header.component";
 import { FooterComponent } from "./footer/footer.component";
 import { ApiService } from "./api/api.service";
+import { PublicComponent } from './public/public.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-	declarations: [AppComponent, UploadComponent, DownloadComponent, HeaderComponent, FooterComponent],
+	declarations: [AppComponent, UploadComponent, DownloadComponent, HeaderComponent, FooterComponent, PublicComponent, HomeComponent],
 	imports: [BrowserModule, AppRoutingModule, HttpClientModule],
 	providers: [ApiService],
 	bootstrap: [AppComponent],
